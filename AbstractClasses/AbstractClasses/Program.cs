762dd297fb6790392932ae679d69e1d714dfa8d8
Stephen Acquah <sacquah048@st.ug.edu.gh>
2026-09-05 13:53:24 +0000

    Add Abstract Classes and Methods task

---
+++ AbstractClasses/AbstractClasses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+class Rectangle : Shape
+{
+    public double Length { get; set; }
+    public double Width { get; set; }
+
+    public Rectangle(double length, double width)
+    {
+        Length = length;
+        Width = width;
+    }
+
+    public override double GetArea()
+    {
+        return Length * Width;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(10, 4);
+
+        Console.WriteLine("Area of Circle: " + circle.GetArea());
+        Console.WriteLine("Area of Rectangle: " + rectangle.GetArea());
+    }
+}
